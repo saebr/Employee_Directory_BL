cbd9582412355553a4d2607798be9dbda9e47d03
Saebr <bensaebr@gmail.com>
2021-05-19 19:28:45 -0500

    Added style to main page and started adding functionallity to app

---
+++ src/components/Hero/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Hero(props) {
+  return (
+    <div className="hero text-center">
+      <h1>Employee Directory</h1>
+      <h2 class="mt-5">Use the search box to narrow your results.</h2>
+    </div>
+  );
+}
+
+export default Hero;
